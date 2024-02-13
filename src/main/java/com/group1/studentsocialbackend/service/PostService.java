9680c7fbe4c97fc3ecb77d330fe0a5ec0e0f552a
Bart <1679243134@qq.com>
2024-02-13 15:18:52 +0100

    Added the home page post logic, can return posts based on page query.

--- src/main/java/com/group1/studentsocialbackend/service/PostService.java
+++ src/main/java/com/group1/studentsocialbackend/service/PostService.java
@@ -1,6 +1,8 @@
 package com.group1.studentsocialbackend.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.group1.studentsocialbackend.PO.Post;
 
 import com.group1.studentsocialbackend.mapper.PostMapper;
@@ -25,6 +27,20 @@ public class PostService {
 
     public List<Post> getListOfUserPosts(String id) {
         return postMapper.selectList(new QueryWrapper<Post>().eq("creator_id", id));
+    }
+
+    public List<Post> getListOfPostsByTimeline(Integer currentPage) {
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("update_time", "create_time");
+        Page<Post> pages = new Page<>(currentPage, 10);
+        IPage<Post> iPage = postMapper.selectPage(pages, queryWrapper);
+        //System.out.println("参数: " + currentPage);
+        System.out.println("总页数: " + iPage.getPages());
+        System.out.println("总个数: " + iPage.getTotal());
+        System.out.println("当前页: " + iPage.getCurrent());
+        List<Post> postList = iPage.getRecords();
+        postList.forEach(System.out::println);
+        return postList;
     }
 
 }
