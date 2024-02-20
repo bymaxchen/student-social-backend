b82c1b4e004e4d9b0a0a1a6ed87558b3749293f3
Bart <1679243134@qq.com>
2024-02-20 15:33:05 +0100

    Modify PicUpload to relative path.

--- src/main/java/com/group1/studentsocialbackend/controller/PictureController.java
+++ src/main/java/com/group1/studentsocialbackend/controller/PictureController.java
@@ -1,44 +1,38 @@
 package com.group1.studentsocialbackend.controller;
 
-import com.group1.studentsocialbackend.PO.Post;
 import com.group1.studentsocialbackend.util.FileNameUtil;
-import com.group1.studentsocialbackend.util.FileUploadUtil;
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.ibatis.annotations.Result;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @RestController
 @RequestMapping("/api/pictures")
 public class PictureController {
 
-    @RequestMapping(value="/upload",method= {RequestMethod.POST,RequestMethod.GET})
-    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
-        //定义要上传文件 的存放路径
-        //replace the localPath with your own accessible computer path!
-        String localPath="/Users/zhangxiaoran/Downloads/";
-        //获得文件名字
-        String fileName=file.getOriginalFilename();
-        fileName= FileNameUtil.getFileName(fileName);
-        File dest = new File(localPath + fileName);
-        if(FileUploadUtil.upload(file, localPath, fileName)){
-            // 将上传的文件写入到服务器端文件夹
-            // 获取当前项目运气的完整url
-            String requestURL = request.getRequestURL().toString();
-            // 获取当前项目的请求路径url
-            String requestURI = request.getRequestURI();
-            // 得到去掉了uri的路径
-            String url = requestURL.substring(0, requestURL.length()-requestURI.length() + 1);
-            url="images/"+ fileName;
+    private static final String UPLOADED_FOLDER = "src/main/resources/static/images/";
 
-
-            return  new ResponseEntity<>(url, HttpStatus.OK);
-
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return new ResponseEntity<>("Please select a file to upload.", HttpStatus.BAD_REQUEST);
         }
-        // 返回
-        return  new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
+
+        try {
+            byte[] bytes = file.getBytes();
+            String filename=FileNameUtil.getFileName(file.getOriginalFilename());
+            Path path = Paths.get(UPLOADED_FOLDER + filename);
+            Files.write(path, bytes);
+            return new ResponseEntity<>("images/"+filename, HttpStatus.OK);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Failed to upload '" + file.getOriginalFilename() + "'", HttpStatus.BAD_REQUEST);
+        }
     }
 }
